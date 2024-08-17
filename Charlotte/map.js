5c22481f418e9c69dcf31f6a35b5d51a8691afea
Risekk37 <ktkim8461@gmail.com>
2024-08-17 04:38:14 -0400

    Add census data
    add Charlotte, New Orleans, Newark&Jersey City, San_francisco

--- Charlotte/map.js
+++ Charlotte/map.js
@@ -70,8 +70,8 @@ map.on('load', function () {
         'paint': {
             'fill-color': [
                 'case',
-                ['==', ['get', 'ACSDT5Y2022.B25077-Data_Estimate!!Median value (dollars)'], null], 'transparent',
-                ['step', ['get', 'ACSDT5Y2022.B25077-Data_Estimate!!Median value (dollars)'],
+                ['==', ['get', 'ACSDT5Y2020.B25077-Data_Estimate!!Median value (dollars)'], null], 'transparent',
+                ['step', ['get', 'ACSDT5Y2020.B25077-Data_Estimate!!Median value (dollars)'],
                     '#ffffff',
                     0, '#011356',
                     160000, '#01417F',
@@ -108,6 +108,96 @@ map.on('load', function () {
             }
         });
         map.setLayoutProperty('race+poverty', 'visibility', 'none');
+
+        map.addLayer({
+            'id': 'poverty+property',
+            'type': 'fill',
+            'source': {
+                'type': 'geojson',
+                'data': 'data/binarymap/binarymap_Poverty_Property.geojson'
+            },
+            'paint': {
+                'fill-color': [
+                    'match',
+                    ['get', 'Bi2_class'],
+                    'E1', '#F52AFF','E2', '#F254FF','E3', '#F07EFF','E4', '#EDA8FF','E5', '#EAD2FF',
+                    'D1', '#C42DCD','D2', '#C259D2','D3', '#C084D7','D4', '#BEB0DC','D5', '#BBDBE1',
+                    'C1', '#93309B','C2', '#915DA5','C3', '#908AAF','C4', '#8EB7B8','C5', '#8CE4C2',
+                    'B1', '#623468','B2', '#616277','B3', '#609186','B4', '#5FBF95','B5', '#5EEDA4',
+                    'A1', '#313736','A2', '#30664A','A3', '#30975E','A4', '#2FC671','A5', '#2FF685',
+                    '#000000' 
+                    ]
+                }
+            });
+            map.setLayoutProperty('poverty+property', 'visibility', 'none');    
+    
+        map.addLayer({
+            'id': 'race+property',
+            'type': 'fill',
+            'source': {
+                'type': 'geojson',
+                'data': 'data/binarymap/binarymap_Race_Property.geojson'
+            },
+            'paint': {
+                'fill-color': [
+                    'match',
+                    ['get', 'Bi3_class'],
+                    'E1', '#F52AFF','E2', '#F254FF','E3', '#F07EFF','E4', '#EDA8FF','E5', '#EAD2FF',
+                    'D1', '#C42DCD','D2', '#C259D2','D3', '#C084D7','D4', '#BEB0DC','D5', '#BBDBE1',
+                    'C1', '#93309B','C2', '#915DA5','C3', '#908AAF','C4', '#8EB7B8','C5', '#8CE4C2',
+                    'B1', '#623468','B2', '#616277','B3', '#609186','B4', '#5FBF95','B5', '#5EEDA4',
+                    'A1', '#313736','A2', '#30664A','A3', '#30975E','A4', '#2FC671','A5', '#2FF685',
+                    '#000000' 
+                    ]
+                }
+            });
+            map.setLayoutProperty('race+property', 'visibility', 'none');    
+
+                
+            map.addLayer({
+                'id': 'poverty+property',
+                'type': 'fill',
+                'source': {
+                    'type': 'geojson',
+                    'data': 'data/binarymap/binarymap_Poverty_Property.geojson'
+                },
+                'paint': {
+                    'fill-color': [
+                        'match',
+                        ['get', 'Bi2_class'],
+                        'E1', '#F52AFF','E2', '#F254FF','E3', '#F07EFF','E4', '#EDA8FF','E5', '#EAD2FF',
+                        'D1', '#C42DCD','D2', '#C259D2','D3', '#C084D7','D4', '#BEB0DC','D5', '#BBDBE1',
+                        'C1', '#93309B','C2', '#915DA5','C3', '#908AAF','C4', '#8EB7B8','C5', '#8CE4C2',
+                        'B1', '#623468','B2', '#616277','B3', '#609186','B4', '#5FBF95','B5', '#5EEDA4',
+                        'A1', '#313736','A2', '#30664A','A3', '#30975E','A4', '#2FC671','A5', '#2FF685',
+                        '#000000' 
+                        ]
+                    }
+                });
+                map.setLayoutProperty('poverty+property', 'visibility', 'none');    
+            
+                map.addLayer({
+                    'id': 'race+property',
+                    'type': 'fill',
+                    'source': {
+                        'type': 'geojson',
+                        'data': 'data/binarymap/binarymap_Race_Property.geojson'
+                    },
+                    'paint': {
+                        'fill-color': [
+                            'match',
+                            ['get', 'Bi3_class'],
+                            'E1', '#F52AFF','E2', '#F254FF','E3', '#F07EFF','E4', '#EDA8FF','E5', '#EAD2FF',
+                            'D1', '#C42DCD','D2', '#C259D2','D3', '#C084D7','D4', '#BEB0DC','D5', '#BBDBE1',
+                            'C1', '#93309B','C2', '#915DA5','C3', '#908AAF','C4', '#8EB7B8','C5', '#8CE4C2',
+                            'B1', '#623468','B2', '#616277','B3', '#609186','B4', '#5FBF95','B5', '#5EEDA4',
+                            'A1', '#313736','A2', '#30664A','A3', '#30975E','A4', '#2FC671','A5', '#2FF685',
+                            '#000000' 
+                            ]
+                        }
+                    });
+                    map.setLayoutProperty('race+property', 'visibility', 'none');    
+            
 
     map.addLayer({
         'id': 'race_2020_stroke',
@@ -1048,6 +1138,24 @@ const matrixData = [
     ["#000000", "#003A04", "#006B1D", "#009D36", "#00CE4E", "#00FF67"],
 ];
 
+const matrix2Data = [
+    ["#F800FF", "#F52AFF", "#F254FF", "#F07EFF", "#EDA8FF", "#EAD2FF"],
+    ["#C500D1", "#C42DCD", "#C259D2", "#C084D7", "#BEB0DC", "#BBDBE1"],
+    ["#9300A3", "#93309B", "#915DA5", "#908AAF", "#8EB7B8", "#8CE4C2"],
+    ["#610075", "#623468", "#616277", "#609186", "#5FBF95", "#5EEDA4"],
+    ["#2E0047", "#313736", "#30664A", "#30975E", "#2FC671", "#2FF685"],
+    ["#000000", "#003A04", "#006B1D", "#009D36", "#00CE4E", "#00FF67"],
+];
+
+const matrix3Data = [
+    ["#F800FF", "#F52AFF", "#F254FF", "#F07EFF", "#EDA8FF", "#EAD2FF"],
+    ["#C500D1", "#C42DCD", "#C259D2", "#C084D7", "#BEB0DC", "#BBDBE1"],
+    ["#9300A3", "#93309B", "#915DA5", "#908AAF", "#8EB7B8", "#8CE4C2"],
+    ["#610075", "#623468", "#616277", "#609186", "#5FBF95", "#5EEDA4"],
+    ["#2E0047", "#313736", "#30664A", "#30975E", "#2FC671", "#2FF685"],
+    ["#000000", "#003A04", "#006B1D", "#009D36", "#00CE4E", "#00FF67"],
+];
+
 // SVG的尺寸和边距
 const margin = { top: 20, right: 20, bottom: 30, left: 32},
         width = 262 - margin.left - margin.right,
@@ -1055,6 +1163,19 @@ const margin = { top: 20, right: 20, bottom: 30, left: 32},
 
 // 创建SVG元素
 const svg = d3.select('#matrix').append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+
+
+const svg2 = d3.select('#matrix2').append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+
+const svg3 = d3.select('#matrix3').append('svg')
         .attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom)
         .append('g')
@@ -1074,6 +1195,29 @@ matrixData.forEach((row, i) => {
         .style('fill', color);
     });
 });
+
+matrix2Data.forEach((row, i) => {
+    row.forEach((color, j) => {
+    svg2.append('rect')
+        .attr('x', j * blockSize)
+        .attr('y', i * blockSize)
+        .attr('width', blockSize)
+        .attr('height', blockSize)
+        .style('fill', color);
+    });
+});
+
+matrix3Data.forEach((row, i) => {
+    row.forEach((color, j) => {
+    svg3.append('rect')
+        .attr('x', j * blockSize)
+        .attr('y', i * blockSize)
+        .attr('width', blockSize)
+        .attr('height', blockSize)
+        .style('fill', color);
+    });
+});
+
 
 // 创建左侧和底部的百分比标签
 const yAxisValues = d3.range(60, -1, -12);
@@ -1101,6 +1245,59 @@ xAxisValues.forEach((value, i) => {
        .style('text-anchor', 'middle') // 文本水平居中
        .text(`${value}%`); // 添加文本
 });
+
+const yAxisValues2 = d3.range(60, -1, -12);
+const xAxisValues2 = d3.range(0, 101, 20);
+
+// 添加Y轴百分比标签
+yAxisValues2.forEach((value, i) => {
+    svg2.append('text')
+       .attr('x', margin.left-36) // 标签在左侧的位置
+       .attr('y', i * blockSize + blockSize / 2) // 标签在每个方块中间的Y位置
+       .style('fill', 'white') // 文本颜色
+       .style('font-size', '14px')
+       .style('alignment-baseline', 'middle') // 文本垂直居中
+       .style('text-anchor', 'end') // 文本靠右对齐
+       .text(`${value}%`); // 添加文本
+});
+
+// 添加X轴百分比标签
+xAxisValues2.forEach((value, i) => {
+    svg2.append('text')
+       .attr('x', i * blockSize + blockSize / 2) // 标签在每个方块中间的X位置
+       .attr('y', height + margin.bottom / 2) // 标签在底部的位置
+       .style('fill', 'white') 
+       .style('font-size', '14px')// 文本颜色
+       .style('text-anchor', 'middle') // 文本水平居中
+       .text(`${value}%`); // 添加文本
+});
+
+const yAxisValues3 = d3.range(60, -1, -12);
+const xAxisValues3 = d3.range(0, 101, 20);
+
+// 添加Y轴百分比标签
+yAxisValues3.forEach((value, i) => {
+    svg3.append('text')
+       .attr('x', margin.left-36) // 标签在左侧的位置
+       .attr('y', i * blockSize + blockSize / 2) // 标签在每个方块中间的Y位置
+       .style('fill', 'white') // 文本颜色
+       .style('font-size', '14px')
+       .style('alignment-baseline', 'middle') // 文本垂直居中
+       .style('text-anchor', 'end') // 文本靠右对齐
+       .text(`${value}%`); // 添加文本
+});
+
+// 添加X轴百分比标签
+xAxisValues3.forEach((value, i) => {
+    svg3.append('text')
+       .attr('x', i * blockSize + blockSize / 2) // 标签在每个方块中间的X位置
+       .attr('y', height + margin.bottom / 2) // 标签在底部的位置
+       .style('fill', 'white') 
+       .style('font-size', '14px')// 文本颜色
+       .style('text-anchor', 'middle') // 文本水平居中
+       .text(`${value}%`); // 添加文本
+});
+
 
 function checkSelection() {
     // 获取每个复选框的状态
@@ -1133,7 +1330,32 @@ function checkSelection() {
     matrixElement.style.display = 'none';
     map.setLayoutProperty('race+poverty', 'visibility', 'none');
     }
+
+    // 显示或隐藏matrix元素
+    const matrixElement2 = document.getElementById('matrix2');
+    if (housingChecked && povertyChecked) {
+      // 如果race和poverty都被选中，则显示matrix元素
+    matrixElement2.style.display = 'block';
+    map.setLayoutProperty('poverty+property', 'visibility', 'visible'); 
+    } else {
+    // 否则隐藏matrix元素
+    matrixElement2.style.display = 'none';
+    map.setLayoutProperty('poverty+property', 'visibility', 'none');
+    }
+
+    // 显示或隐藏matrix元素
+    const matrixElement3 = document.getElementById('matrix3');
+    if (housingChecked && raceChecked) {
+      // 如果race和poverty都被选中，则显示matrix元素
+    matrixElement3.style.display = 'block';
+    map.setLayoutProperty('race+property', 'visibility', 'visible'); 
+    } else {
+    // 否则隐藏matrix元素
+    matrixElement3.style.display = 'none';
+    map.setLayoutProperty('race+property', 'visibility', 'none');
+    }
 }
+
 
 // 为复选框添加事件监听器
 document.getElementById('race2020').addEventListener('change', checkSelection);
@@ -1145,4 +1367,5 @@ document.getElementById('housing2020').addEventListener('change', checkSelection
     document.getElementById('race2020').checked = true;  
     document.getElementById('poverty2020').checked = false;
     document.getElementById('housing2020').checked = false;
-
+    document.getElementById('matrix2').style.display = 'none'; 
+    document.getElementById('matrix3').style.display = 'none'; 
